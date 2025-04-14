f790b0bddfd87d754ccf1b1f2d031f560cdf9a84
bazik26 <bazikbazik26@gmail.com>
2025-04-14 15:28:07 +0300

    fixyandexpixel

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -37,61 +37,6 @@ function App({ Component, pageProps }: AppProps) {
   return (
     mounted && (
       <>
-        {/* Яндекс.Метрика */}
-        <Script id="yandex-metrika" strategy="afterInteractive">
-          {`
-            (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)}; 
-            m[i].l=1*new Date();
-            for (var j = 0; j < document.scripts.length; j++) 
-            { if (document.scripts[j].src.includes("mc.yandex.ru")) return; }
-            k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-            (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
-
-            ym(100105008, "init", {
-              clickmap:true,
-              trackLinks:true,
-              accurateTrackBounce:true
-            });
-          `}
-        </Script>
-
-        {/* Fallback для пользователей без JS */}
-        <noscript>
-          <div>
-            <img
-              src="https://mc.yandex.ru/watch/100105008"
-              style={{ position: 'absolute', left: '-9999px' }}
-              alt="Yandex Metrika"
-            />
-          </div>
-        </noscript>
-
-        {/* Подключаем пиксель Top.Mail.Ru */}
-        <Script id="top-mail-ru" strategy="lazyOnload">
-          {`
-            var _tmr = window._tmr || (window._tmr = []);
-            _tmr.push({id: "3618651", type: "pageView", start: (new Date()).getTime()});
-            (function (d, w, id) {
-              if (d.getElementById(id)) return;
-              var ts = d.createElement("script"); ts.type = "text/javascript"; ts.async = true; ts.id = id;
-              ts.src = "https://top-fwz1.mail.ru/js/code.js";
-              var f = function () {var s = d.getElementsByTagName("script")[0]; s.parentNode.insertBefore(ts, s);};
-              if (w.opera == "[object Opera]") { d.addEventListener("DOMContentLoaded", f, false); } else { f(); }
-            })(document, window, "tmr-code");
-          `}
-        </Script>
-
-        {/* Fallback для пользователей без JS */}
-        <noscript>
-          <div>
-            <img
-              src="https://top-fwz1.mail.ru/counter?id=3618651;js=na"
-              style={{ position: 'absolute', left: '-9999px' }}
-              alt="Top.Mail.Ru"
-            />
-          </div>
-        </noscript>
-
         <NextNProgress />
         <Component {...pageProps} />
         <ToastContainer
