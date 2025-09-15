8a56cfb40086e81b92d00cfa79ccdbf23f92a410
Bazik26 <bazikbazik26@gmail.com>
2025-09-15 09:35:54 +0300

    fix ya map

--- components/elements/YandexMap/YandexMap.tsx
+++ components/elements/YandexMap/YandexMap.tsx
@@ -1,7 +1,7 @@
 const YandexMap = () => {
     return (
       <div style={{ width: '100%', height: '400px' }}>
-        <iframe src="https://yandex.ru/map-widget/v1/?um=constructor%3A088e5115e13f7c092ccce6b24f90ae7e1ac112535bf074dbe5d69be533f08dc6&amp;source=constructor" width="100%" height="400" frameBorder="0"></iframe>
+        <iframe src="https://yandex.ru/map-widget/v1/?um=constructor%3A698ae0a2ae2a5e8fed346f92de9d51527ff4b747dec7a0f516ba563b47e34d83&amp;source=constructor" width="100%" height="400" frameBorder="0"></iframe>
       </div>
     );
   };
